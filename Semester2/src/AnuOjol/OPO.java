5ee5ffcb8ca9131179d4f437db333b4657dae79a
kemal003-gitHubDesktop <60648376+kemal003@users.noreply.github.com>
2020-05-14 10:54:15 +0700

    Fix

--- Semester2/src/AnuOjol/OPO.java
+++ Semester2/src/AnuOjol/OPO.java
@@ -7,8 +7,11 @@ public class OPO {
     }
 
     public void tambahSaldo(int tambahSaldo) {
-        isiSaldo += tambahSaldo;
-        if (tambahSaldo!=0) System.out.printf("Berhasil TopUp sebesar: Rp. %,d\n", tambahSaldo);
+        if (tambahSaldo>=0) {
+            System.out.printf("Berhasil TopUp sebesar: Rp. %,d\n", tambahSaldo);
+            isiSaldo += tambahSaldo;
+        }
+        else System.out.println("Gagal, TopUp salah");
         System.out.printf("Saldo sekarang: Rp. %,d\n", isiSaldo);
     }
 
--- Semester2/src/AnuOjol/PengemudiMobil.java
+++ Semester2/src/AnuOjol/PengemudiMobil.java
@@ -23,3 +23,4 @@ public class PengemudiMobil extends Pengemudi{
         return biayaSatuan;
     }
 }
+
